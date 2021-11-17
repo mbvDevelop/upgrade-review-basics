705ffea325cd3dfddf4fe4ad4c1e1fb2ca96fe21
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 16:00:27 +0100

    second commit

--- main.js
+++ main.js
@@ -15,3 +15,49 @@ for (movie of movies) {
 }
  
 console.log(categories)
+
+ 
+// Iteration 2
+ 
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+let sum = 0
+let count = 0
+for (user of users) {
+    console.log(user)
+    for(sound in user.favoritesSounds) {
+        console.log(user.favoritesSounds[sound].volume)
+        sum += user.favoritesSounds[sound].volume
+        count++
+    }
+}
+let average = sum /  count
+console.log(average)
