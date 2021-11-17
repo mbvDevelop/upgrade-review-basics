ceca07c534ff58b35d2350291baae0e906dbc86a
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 15:59:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Iteracion 1
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+let categories = []
+for (movie of movies) {
+    for (category of movie.categories) {
+        if (!categories.includes(category)) {
+            categories.push(category)
+        }
+    }
+}
+ 
+console.log(categories)
