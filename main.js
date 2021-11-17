1d1b9a601001f16b7d79493d432adb9476e34329
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 16:02:47 +0100

    fifth commit

--- main.js
+++ main.js
@@ -120,3 +120,11 @@ function findArrayIndex(array, text) {
 }
  
 console.log(findArrayIndex(array, "Mosquito"))
+
+// Iteracion 5
+ 
+function rollDice(faces) {
+    return Math.floor((Math.random() * faces ) + 1)
+}
+ 
+console.log(rollDice(6))
